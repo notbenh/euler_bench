90668b8d801e42d5d256a86e487a2a5ace9d718b
Jeff Lavallee <jeff@zeroclue.com>
2009-07-11 19:53:44 -0700

    solutions for problems 4 and 8 in Perl 5 and 6

---
+++ perl5/004.01.pl
@@ -0,0 +1,23 @@
+#!/usr/local/bin/perl
+
+use strict; use warnings;
+
+# Problem 4
+# 16 November 2001
+# 
+# A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 99.
+# 
+# Find the largest palindrome made from the product of two 3-digit numbers.
+
+
+my $max = 0;
+for my $a ( 100..999 ){
+    for my $b ( 100..999 ){
+        my $product = $a * $b;
+
+        $max = $product if $product > $max
+                       and $product eq reverse $product;
+    }
+}
+
+print "$max\n";
