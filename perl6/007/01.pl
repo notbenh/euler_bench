c8590b7c16945ed0fdde8d6ff7f3c8ba6d6f4b37
Duke Leto <jonathan@leto.net>
2009-07-24 19:13:20 -0700

    is_prime() only needs to check to sqrt($n)

--- perl6/007/01.pl
+++ perl6/007/01.pl
@@ -2,7 +2,7 @@ my $prime_cnt = 2;
 my $i = 3;
 while ( $prime_cnt < 10001 ) {
     $i += 2;
-    if ( test_prime($i) ) {
+    if ( is_prime($i) ) {
         $prime_cnt++;
         #say "$i is prime ($prime_cnt)";
     }
@@ -10,10 +10,10 @@ while ( $prime_cnt < 10001 ) {
 say $i;
 
 
-sub test_prime($n) {
+sub is_prime( $n) {
 
     my $x = 3;
-    while ( $x < $n ) {
+    while ( $x < sqrt $n ) {
         return 0 unless $n % $x;
         $x += 2;
     }
