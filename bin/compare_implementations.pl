22f95998dd98fd08747b907b022c756a361b3b00
Duke Leto <jonathan@leto.net>
2009-08-02 01:09:58 -0700

    Inital benchmark script compare_implementations.pl
    This script compares implementations given a language and Euler Project
    number. It defaults to parrot implementation 001.

---
+++ bin/compare_implementations.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/env perl5.10
+# Hello. I compare units of code for the Euler Bench Project
+use 5.010;
+use strict;
+use warnings;
+use Data::Dumper;
+use Benchmark qw/:all/;
+use File::Spec::Functions;
+
+# we should change perl6 to rakudo
+my @languages = qw/ parrot perl5 perl6 /;
+# Uses the $PARROT env variable or defaults to where I put it :)
+my $parrot = $ENV{PARROT} || catfile( $ENV{HOME},qw{git parrot parrot});
+
+my ($profile_lang,$euler_problem,$count) = @ARGV;
+$profile_lang ||= 'parrot';
+$euler_problem ||= '001';
+$count ||= 1000;
+
+
+my $code_dir = catdir($profile_lang,$euler_problem);
+die "Euler Project Problem #$euler_problem for $profile_lang does not exist!" unless -e $code_dir;
+my @codez = glob catdir($code_dir,'*');
+
+my %bench_data =
+    map {
+        my $file = $_;
+        $file => sub { system("$parrot $file &>/dev/null") }
+    } @codez;
+
+#die Dumper [ %bench_data ];
+say "Benchmarking $profile_lang on EP#$euler_problem with $count iterations";
+cmpthese($count, \%bench_data);
+
