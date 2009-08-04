e4bcd959c78e76df9ab59c0b835a3ffd5dbe204e
SF <Last.Of.The.Careless.Men@gmail.com>
2009-08-03 21:40:41 -0400

    Optimized version of 01.pl.
    This swaps the actual strings for the number names with their lengths, and lifts the sorting out of the inner loop.

--- perl6/017/02.pl
+++ perl6/017/02.pl
@@ -14,65 +14,63 @@ use v6;
 # than the actual string.  That's harder to test, but would be vastly more
 # efficient.
 
-my %number_names = (
-    1 => "one",
-    2 => "two",
-    3 => "three",
-    4 => "four",
-    5 => "five",
-    6 => "six",
-    7 => "seven",
-    8 => "eight",
-    9 => "nine",
-    10 => "ten",
-    11 => "eleven",
-    12 => "twelve",
-    13 => "thirteen",
-    14 => "fourteen",
-    15 => "fifteen",
-    16 => "sixteen",
-    17 => "seventeen",
-    18 => "eighteen",
-    19 => "nineteen",
-    20 => "twenty",
-    30 => "thirty",
-    40 => "forty",
-    50 => "fifty",
-    60 => "sixty",
-    70 => "seventy",
-    80 => "eighty",
-    90 => "ninety"
+my %number_lengths = (
+    1 => 3,
+    2 => 3,
+    3 => 5,
+    4 => 4,
+    5 => 4,
+    6 => 3,
+    7 => 5,
+    8 => 5,
+    9 => 4,
+    10 => 3,
+    11 => 6,
+    12 => 6,
+    13 => 8,
+    14 => 8,
+    15 => 7,
+    16 => 7,
+    17 => 9,
+    18 => 8,
+    19 => 8,
+    20 => 6,
+    30 => 6,
+    40 => 5,
+    50 => 5,
+    60 => 5,
+    70 => 7,
+    80 => 6,
+    90 => 6
 );
 
 for 1..9 -> $n
 {
-    %number_names{$n * 100} = %number_names{$n} ~ " hundred";
+    %number_lengths{$n * 100} = %number_lengths{$n} + 7;
 }
 
-sub NumberName(%number_names, $number)
+sub NumberLength(@numbers, %number_lengths, $number)
 {
     given $number
     {
-        when 0 { return "zero"; }
-        when 1000 { return "one thousand"; }
+        when 0 { return 4; }
+        when 1000 { return 11; }
     }
     my $n = $number;
-    my $name = "";
+    my $length = 0;
     while ($n > 0)
     {
-        $name ~= " " if $name.chars > 0;
-        my $biggest = %number_names.keys.grep({$_ <= $n}).map({+$_}).max;
-        $name ~= %number_names{$biggest};
+        my $biggest = @numbers.grep({$_ <= $n}).max;
+        $length += %number_lengths{$biggest};
         $n -= $biggest;
     }
-    return $name;   
+    return $length;   
 }
 
+my @numbers = %number_lengths.keys.map({+$_});
 my $letter = 0;
 for 1..1000 -> $number
 {
-    my $name = NumberName(%number_names, $number);
-    # say "$number => $name";
-    $name.subst(rx/<alpha>/, { $letter++ }, :g);
+    $letter += NumberLength(@numbers, %number_lengths, $number);
 }
 say $letter;
