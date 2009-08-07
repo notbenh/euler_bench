f3fc0dad84dd35f68cd571c504d3c633399ff0a3
ben hengst <notbenh@cpan.org>
2009-08-06 20:28:44 -0700

    respecting the PERL5 ENV var

--- bin/compare_implementations.pl
+++ bin/compare_implementations.pl
@@ -9,7 +9,7 @@ use File::Spec::Functions;
 
 my %interp = (
     parrot => $ENV{PARROT} || catfile( $ENV{HOME},qw{git parrot parrot}),
-    perl5  => 'perl5.10',
+    perl5  => $ENV{PERL5} || 'perl5.10',
     # we should change perl6 to rakudo
     perl6  => $ENV{PERL6} || catfile( $ENV{HOME},qw{git rakudo perl6}),
 );
