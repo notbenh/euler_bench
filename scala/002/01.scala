7df1c2ea3377b106e63fc857f233a9bbb9ed65e9
Ann Shim <annie@oxygen.local>
2009-10-22 01:28:55 -0700

    now handles longs

--- scala/002/01.scala
+++ scala/002/01.scala
@@ -1,8 +1,8 @@
 // a tail recursive fib implementation
 // http://en.literateprograms.org/Fibonacci_numbers_%28Scala%29
-def fib(n: Int): Int = fib_tr(n, 1, 0) 
+def fib(n: Int): Long = fib_tr(n, 1, 0) 
 
-def fib_tr(n: Int, b: Int, a: Int): Int = n match {
+def fib_tr(n: Int, b: Long, a: Long): Long = n match {
     case 0 => a 
     case _ => fib_tr(n - 1, a + b, b)
 }
@@ -20,7 +20,6 @@ def acc_tr_helper(n: Int, sum: Int): Int = n match {
 val maxAllowedValue = 4000000
 // perhaps a Projection to infinity instead?
 val fibLessThanMax = (1 to Integer.MAX_VALUE).map(fib(_)).takeWhile(_ < maxAllowedValue)
-val sum = fibLessThanMax.filter(_ % 2 == 0).foldLeft(0)(_ + _)
-println(sum)
+println(fibLessThanMax.filter(_ % 2 == 0).foldLeft(0L)(_ + _))
 
 // http://oldfashionedsoftware.com/2009/07/30/lots-and-lots-of-foldleft-examples/
