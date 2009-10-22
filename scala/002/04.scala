4e45051a883c1e5ad0fd148943a69f120b993574
Ann Shim <annie@oxygen.local>
2009-10-21 22:32:50 -0700

    working!

--- scala/002/04.scala
+++ scala/002/04.scala
@@ -1,11 +1,26 @@
+// a tail recursive fib implementation
 // http://en.literateprograms.org/Fibonacci_numbers_%28Scala%29
+def fib(n: Int): Int = fib_tr(n, 1, 0) 
 
-  def fib( n:Long) = fib_tr( n, 1, 0) 
-
-  def fib_tr( n: Long, b: Long, a: Long): Long = n match {
-
+def fib_tr(n: Int, b: Int, a: Int): Int = n match {
     case 0 => a 
-    case _ => fib_tr( n -1, a + b, b)
-  }
+    case _ => fib_tr(n - 1, a + b, b)
+}
 
-  println(fib(40))
+// experimentations in summations (and tail recursion)
+def acc_tr(n: Int): Int = {
+  acc_tr_helper(n, 0)
+}
+
+def acc_tr_helper(n: Int, sum: Int): Int = n match {
+  case 0 => sum
+  case _ => acc_tr_helper(n - 1, n + sum)
+}
+
+val maxAllowedValue = 4000000
+// perhaps a Projection to infinity instead?
+val fibLessThanMax = (1 to Integer.MAX_VALUE).map(fib(_)).takeWhile(_ < maxAllowedValue)
+val sum = fibLessThanMax.filter(_ % 2 == 0).foldLeft(0)(_ + _)
+println(sum)
+
+// http://oldfashionedsoftware.com/2009/07/30/lots-and-lots-of-foldleft-examples/
