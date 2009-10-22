31cfc198e88adaff5eed42778b845454e5239076
Ann Shim <annie@oxygen.local>
2009-10-22 13:43:19 -0700

    found out I needed to sort

--- scala/004/01.scala
+++ scala/004/01.scala
@@ -10,5 +10,16 @@ def expandRange(x: Int): Seq[Int] = {
     range.map(_ * x)
 }
 
+def expandRangeOnlyPals(x: Int): Seq[Int] = {
+    val range = 999 to 1 by -1
+    range.map(_ * x).filter(isPalindrome(_))
+}
+
 val range = 999 to 1 by -1
-println(range.flatMap(expandRange(_)).filter(isPalindrome(_)).first)
+// val pals = range.flatMap(expandRange(_)).filter(isPalindrome(_))
+
+val pals = range.flatMap(expandRangeOnlyPals(_))
+// why don't things stay in order out of flatMap? we're going 999, 998 etc
+// so this sort shouldn't be required
+println(scala.util.Sorting.stableSort(pals).last)
+
