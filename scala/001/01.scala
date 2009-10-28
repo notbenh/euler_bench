0bb500fe8bc59613f2c305f9b62fb3c7374542e1
Trenton Lipscomb <trenton@oxygen.local>
2009-10-27 18:46:22 -0700

    fixed %

--- scala/001/01.scala
+++ scala/001/01.scala
@@ -9,7 +9,7 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 
 var sum = 0
 
-for (i <- 0 until 1000 if i % 3 == 0 || i % 3 == 5) {
+for (i <- 0 until 1000 if i % 3 == 0 || i % 5 == 0) {
     sum += i
 }
 
