c1e6a0a430018e5bac938457590c4fdae39ae289
Trenton Lipscomb <trentonl@gmail.com>
2009-10-28 22:53:24 -0700

    replaced sums with foldLefts

--- scala/006/01.scala
+++ scala/006/01.scala
@@ -1,17 +1,13 @@
 
 def sumOfSquares(xs: Seq[Int]): Long = {
-  // can't use foldLeft b/c it's return type must match the type of 
-  // elements in the list (why?????)
-  var sum = 0L
-  xs.foreach((x) => sum += Math.round(Math.pow(x, 2)))
-  sum
+  xs.foldLeft(0L)((sum, x) => sum + Math.round(Math.pow(x, 2)))
 }
 
 def squareOfSum(xs: Seq[Int]): Long = {
-  var sum = 0L
-  xs.foreach((x) => sum += x)
+  var sum = xs.foldLeft(0L)((sum, x) => sum + x)
   Math.round(Math.pow(sum, 2))
 }
 
 val range = 1 to 100
 println(squareOfSum(range) - sumOfSquares(range))
+
