155784bef6069668507975ad2d4417ef71890b96
Trenton Lipscomb <trentonl@gmail.com>
2009-10-29 14:59:34 -0700

    subclassed BigInt and added ! operator

--- scala/020/01.scala
+++ scala/020/01.scala
@@ -1,14 +1,19 @@
-def fact(x: BigInt): BigInt = {
-  if(x == BigInt(0)) {
-    BigInt(0)
-  }
-  else if(x == BigInt(1)) {
-    BigInt(1)
-  }
-  else {
-    x * fact(x - 1)
-  }
+
+object BetterBigInt {
+  def apply(n: Int) = new BetterBigInt(n)
+
+  implicit def int2better(n: Int) = apply(n)
+
+  def factorial(x: Int): BetterBigInt = new BetterBigInt((1 to x).foldLeft(1: BigInt)(_ * _))
 }
 
+class BetterBigInt(val n: java.math.BigInteger) extends BigInt(n) {
+  def this(n: Int) = this(java.math.BigInteger.valueOf(n))
+  def this(n: BigInt) = this(n.bigInteger)
+
+  def ! = BetterBigInt.factorial(n.intValue)
+}
+
+val x: BetterBigInt = 100
 // split has "" as the first element, so we drop it
-println(fact(BigInt(100)).toString.split("").drop(1).foldLeft(0L)(_ + Integer.parseInt(_)))
+println((x!).toString.split("").drop(1).foldLeft(0L)(_ + Integer.parseInt(_)))
