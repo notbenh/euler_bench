40469b16c6e407f29b873ff89857cccfd06b1e15
Trenton Lipscomb <trentonl@gmail.com>
2009-10-28 23:49:11 -0700

    using Stream vs Range

--- scala/007/01.scala
+++ scala/007/01.scala
@@ -1,7 +1,7 @@
 import scala.Math._
 
 // produces a list of divisors of x, in decreasing order, excluding x
-def divisors(x: Long): Seq[Int] = {
+def divisors(x: Int): Seq[Int] = {
   // start at sqrt of x, since that's the largest possible divisor
   val r = round(floor(sqrt(x))).toInt to 1 by -1
   // identify divisors 
@@ -9,10 +9,9 @@ def divisors(x: Long): Seq[Int] = {
 }
 
 // a number is prime iff it's only divisors are itself and 1
-def isPrime(x: Long): Boolean = {
+def isPrime(x: Int): Boolean = {
   divisors(x).length == 1
 }
 
 val nthOne = 10001
-val range = 1 to Integer.MAX_VALUE
-println(range.filter(isPrime(_))(nthOne))
+println(Stream.from(1).filter(isPrime(_))(nthOne))
