e5b2a0821919604e4caf74759eb8a3d330a8df1d
Trenton Lipscomb <trentonl@gmail.com>
2009-10-29 01:44:17 -0700

    working

---
+++ scala/020/01.scala
@@ -0,0 +1,14 @@
+def fact(x: BigInt): BigInt = {
+  if(x == BigInt(0)) {
+    BigInt(0)
+  }
+  else if(x == BigInt(1)) {
+    BigInt(1)
+  }
+  else {
+    x * fact(x - 1)
+  }
+}
+
+// split has "" as the first element, so we drop it
+println(fact(BigInt(100)).toString.split("").drop(1).foldLeft(0L)(_ + Integer.parseInt(_)))
